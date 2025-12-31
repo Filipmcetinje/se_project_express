938e6a48b235993706f2c99ce274d77881ce6024
Filip Milosevic <filipmcetinje@gmail.com>
2025-12-31 12:02:32 -0500

    Final Project 15

--- app.js
+++ app.js
@@ -22,11 +22,11 @@ app.use(cors());
 app.use(express.json());
 app.use(requestLogger);
 
-app.get("/crash-test", () => {
-  setTimeout(() => {
-    throw new Error("Server will crash now");
-  }, 0);
-});
+//app.get("/crash-test", () => {
+//setTimeout(() => {
+//throw new Error("Server will crash now");
+//}, 0);
+//});
 
 app.post("/signup", validateSignup, createUser);
 app.post("/signin", validateLogin, login);
